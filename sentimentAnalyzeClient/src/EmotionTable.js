d543046369c1b827fe24a9ef83db9ecfec888949
snvc00 <snvc00@gmail.com>
2021-07-13 16:14:32 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -5,12 +5,16 @@ class EmotionTable extends React.Component {
     render() {
       return (  
         <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
           <table className="table table-bordered">
             <tbody>
             {
                 //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+                this.props.emotions.map(emotion => (
+                    <tr>
+                        <td>{emotion.label}</td>
+                        <td>{emotion.score}</td>
+                    </tr>
+                ))
             }
             </tbody>
           </table>
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -3,28 +3,83 @@ const app = new express();
 
 app.use(express.static('client'))
 
+require('dotenv').config()
 const cors_app = require('cors');
 app.use(cors_app());
+
+const getNLUInstance = () => {
+    const API_KEY = process.env.API_KEY;
+    const API_URL = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+    const { IamAuthenticator } = require("ibm-watson/auth");
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: "2020-08-01",
+        authenticator: new IamAuthenticator({
+            apikey: API_KEY,
+        }),
+        serviceUrl: API_URL,
+    });
+
+    return naturalLanguageUnderstanding;
+}
 
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
 
-app.get("/url/emotion", (req,res) => {
+app.get("/url/emotion", async (req,res) => {
+    const nlu = getNLUInstance();
+    const response = await nlu.analyze({
+        features: {
+            emotion: {}
+        },
+        url: req.query.url
+    });
 
-    return res.send({"happy":"90","sad":"10"});
+    return res.send({"emotion": response.result.emotion.document.emotion});
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/sentiment", async (req,res) => {
+    const nlu = getNLUInstance();
+    const response = await nlu.analyze({
+        features: {
+            sentiment: {}
+        },
+        url: req.query.url
+    });
+
+    return res.send(response.result.sentiment.document.label);
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/text/emotion", async (req,res) => {
+    const nlu = getNLUInstance();
+    const response = await nlu.analyze({
+        features: {
+            emotion: {}
+        },
+        text: req.query.text
+    });
+
+    const emotions = Object.keys(response.result.emotion.document.emotion).map((emotion, i) => ({
+        label: emotion,
+        score: response.result.emotion.document.emotion[emotion]
+    }));
+
+    return res.send(emotions);
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/sentiment", async (req,res) => {
+    const nlu = getNLUInstance();
+    const response = await nlu.analyze({
+        features: {
+            sentiment: {}
+        },
+        text: req.query.text
+    });
+
+    return res.send(response.result.sentiment.document.label);
 });
 
 let server = app.listen(8080, () => {
